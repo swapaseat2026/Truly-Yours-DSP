368d45d330176ff943896403387a5fdd891f661a
2banguzmusic-lab <2banguzmusic@gmail.com>
2025-12-24 03:48:21 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Wc9me_xmVEorMVSPMhtD9lqQ7P6zjA",
+  authDomain: "truly-yours-dsp-2025.firebaseapp.com",
+  projectId: "truly-yours-dsp-2025",
+  storageBucket: "truly-yours-dsp-2025.firebasestorage.app",
+  messagingSenderId: "369021879682",
+  appId: "1:369021879682:web:ded7b7e150ab8fbb3cbb9b"
+};
+
+// Initialize Firebase (COMPAT)
+firebase.initializeApp(firebaseConfig);
 
