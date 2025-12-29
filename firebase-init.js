b520de9c44cd81f288cf7184ed0ce282d50ba0ff
swapaseat2026 <swapaseat2026@gmail.com>
2025-12-29 13:43:12 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB9b9me_xmEorNFSMPHsTiDP4u7Pc6jfA",
+  apiKey: "AIzaSy....",
   authDomain: "truly-yours-dsp-2025.firebaseapp.com",
   projectId: "truly-yours-dsp-2025",
-  storageBucket: "truly-yours-dsp-2025.firebasestorage.app",  // <-- FIXED
-  messagingSenderId: "369021987682",
-  appId: "1:369021987682:web:dbe97e15e0b8fb8bc3bb09"
+  storageBucket: "truly-yours-dsp-2025.appspot.com", // << FIXED
+  messagingSenderId: "3690...",
+  appId: "1:3690...."
 };
