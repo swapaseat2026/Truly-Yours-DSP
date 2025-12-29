76cdcd6a46aebaa38395a66f30b40a0757ca2804
swapaseat2026 <swapaseat2026@gmail.com>
2025-12-29 13:52:32 -0500

    Delete firebase-init.js

--- firebase-init.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSy....",
-  authDomain: "truly-yours-dsp-2025.firebaseapp.com",
-  projectId: "truly-yours-dsp-2025",
-  storageBucket: "truly-yours-dsp-2025.appspot.com", // << FIXED
-  messagingSenderId: "3690...",
-  appId: "1:3690...."
-};
